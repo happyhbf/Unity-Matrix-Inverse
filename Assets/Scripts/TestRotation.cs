048348bcdb11a3564fce6d7c1e97231b84d756d3
happyhbf <huangbufu@163.com>
2018-06-11 11:15:13 +0800

    演示旋转矩阵以及它的逆矩阵

---
+++ Assets/Scripts/TestRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TestRotation : MonoBehaviour {
+
+	public Transform ObjectForRotation;
+	public Transform ObjectAfterRotation;
+
+	public Transform ObjectForRotationInverse;
+	public Transform ObjectAfterRotationInverse;
+
+	public Vector3 Rotation;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Matrix4x4 m = Matrix4x4.Rotate(Quaternion.Euler(Rotation));
+		ObjectAfterRotation.position = m.MultiplyPoint(ObjectForRotation.position);
+		Matrix4x4 mInv = m.inverse;
+		// 如果ObjectForRotationInverse和ObjectAfterRotation是通一个物体，这边测试结果可以看出ObjectAfterRotationInverse的位置和ObjectForRotation的位置是一样的
+		// 如果ObjectForRotationInverse和ObjectForRotation是通一个物体，这边测试结果可以看出ObjectAfterRotationInverse的运动方向和ObjectAfterRotation运动方向是相反的
+		ObjectAfterRotationInverse.position = mInv.MultiplyPoint(ObjectForRotationInverse.position);
+	}
+}
